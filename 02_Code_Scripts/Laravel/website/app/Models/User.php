26adbbd5d6a0bd3186a4de6f5154d255cf207a81
liobag <lionel.bagnoud@lernende.bfo-vs.ch>
2023-03-07 11:02:38 +0100

    Projekt10

--- 02_Code_Scripts/Laravel/website/app/Models/User.php
+++ 02_Code_Scripts/Laravel/website/app/Models/User.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactorcley, Notifiable;
 
     /**
      * The attributes that are mass assignable.
@@ -42,3 +42,4 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 }
+?>
